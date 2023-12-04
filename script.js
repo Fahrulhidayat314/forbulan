9019bb97a113709510c2e7c27161e66f5eafb439
Fahrulhidayat314 <150500667+Fahrulhidayat314@users.noreply.github.com>
2023-12-04 19:46:16 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,12 +5,23 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-    question.innerHTML = "Aaaaa, I love you too";
-    gif.src =
-        "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; 
+    updateMessage("Aaaaa, I love you too");
+    updateGif("https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp");
 });
 
 noBtn.addEventListener("mouseover", () => {
+    moveNoButtonRandomly();
+});
+
+function updateMessage(message) {
+    question.textContent = message;
+}
+
+function updateGif(source) {
+    gif.src = source;
+}
+
+function moveNoButtonRandomly() {
     const noBtnRect = noBtn.getBoundingClientRect();
     const maxX = window.innerWidth - noBtnRect.width;
     const maxY = window.innerHeight - noBtnRect.height;
@@ -18,6 +29,6 @@ noBtn.addEventListener("mouseover", () => {
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
 
-    noBtn.style.left = randomX + "px"; 
-    noBtn.style.top = randomY + "px"; 
-});
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+}
